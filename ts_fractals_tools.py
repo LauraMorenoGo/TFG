397a21318ef8da87cdbd7bf25587a9ba9209a9ad
Laura <laura.mo.go@hotmail.com>
2018-11-27 21:16:22 +0100

    fallo linea 85
    En la linea 85 tengo un IndentationError pero no sé por qué

--- ts_fractals_tools.py
+++ ts_fractals_tools.py
@@ -15,6 +15,92 @@ from random_walk import *
 
 #compute hurst exponent using rs
 
+
+
+def rs_calc(serie):
+    """
+    function RS = RScalc(rr)
+    %RS compute
+    N = length(rr);
+    mediaDatos = sum(rr)/N; %Data mean
+    X = cumsum(rr-mediaDatos);
+    R = max(X) - min(X); %Range
+    S = sqrt(sum((rr-mediaDatos).^2)/N); %STD
+    RS = R/S; %RS 
+    """
+    N = len(serie)
+    mediaDatos = np.mean(serie) #Data mean
+    X = np.cumsum(serie-mediaDatos)
+    R = np.max(X) - np.min(X) #Range
+    S = np.sqrt(np.mean((serie-mediaDatos)**2)) #STD
+    RS = R/S #RS
+    return RS
+
+
+def myregress(tC,RSave,plotFlag):
+    """
+    function [H, rectaReg, barrasError] = myregress(tC,RSave,plotFlag)
+    x = [ones(length(tC),1) log2(tC)];
+    y = log2(RSave);
+    [c,cint,r,rint,stats] = regress(y,x,0.05);
+    rectaReg = x*c;
+    barrasError = abs(rint(:,1)-r);
+    H = abs(c(2));
+
+    %Plot
+    if plotFlag
+        plot(log2(tC),y,'o-')
+        hold on
+        plot(log2(tC),rectaReg,'r--');
+    end
+    """
+    x = [np.ones((len(tC),1), Float), np.log2(tC)]
+    y = np.log2(RSave)
+    [c,cint,r,rint,stats] = regress(y,x,0.05)
+    rectaReg = x*c
+    barrasError = np.abs(rint[:,0]-r)
+    H = np.abs(c(2))
+
+    #Plot
+    if plotFlag:
+        plot(np.log2(tC),y,'o-')
+        plot(np.log2(tC),rectaReg,'r--')
+        show()
+    return H
+
+rw = random_walk(1100)
+rw = np.diff(rw) #First compute the fGn = diff(fBm)
+
+maxPot2 = 2**np.floor(np.log2(len(rw)))
+datosAux = rw[0:int(maxPot2)];
+tamCaja = int(maxPot2)
+numIter = 1
+RSave = []
+cajas = 1
+tC = [tamCaja]
+while (tamCaja >= 8):
+    inds = []
+    m = 1
+    for l in 0:cajas #Index for this segment lenght
+        indsaux = [m : m+tamCaja-1]
+        m = m + tamCaja
+        inds = [inds; indsaux]   
+    
+    
+    RSiter = []
+    for g in 0:size(inds,1) #RS compute for this segment length
+        RSaux = rs_calc(datosAux(inds[g,:]))
+        RSiter = [RSiter; RSaux]   
+    
+    RSave = [RSave;np.mean(RSiter)]    
+    numIter = numIter + 1
+    tamCaja = tamCaja / 2
+    cajas = 2**(numIter-1)
+    tC = [tC;tamCaja]
+
+tC = tC[0:end-1]
+
+
 """matlab code
 rr = diff(rr); %First compute the fGn = diff(fBm).
 maxPot2 = 2.^floor(log2(length(rr)));
@@ -48,29 +134,3 @@ tC = tC(1:end-1);
 
 
 """
-
-def rs_calc(serie):
-    """
-    function RS = RScalc(rr)
-    %RS compute
-    N = length(rr);
-    mediaDatos = sum(rr)/N; %Data mean
-    X = cumsum(rr-mediaDatos);
-    R = max(X) - min(X); %Range
-    S = sqrt(sum((rr-mediaDatos).^2)/N); %STD
-    RS = R/S; %RS 
-    """
-    N = len(serie)
-    mediaDatos = np.mean(serie) #Data mean
-    X = np.cumsum(serie-mediaDatos)
-    R = np.max(X) - np.min(X) #Range
-    S = np.sqrt(np.mean((serie-mediaDatos)**2)) #STD
-    RS = R/S #RS
-    return RS
-
-
-rw = random_walk(1100)
-rw = np.diff(rw) #First compute the fGn = diff(fBm)
-
-maxPot2 = 2**np.floor(np.log2(len(rw)))
-datosAux = rw[0:int(maxPot2)];
