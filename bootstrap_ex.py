27027b44d1cf756ae593f963ca9c573e5dcc4f7f
Laura <laura.mo.go@hotmail.com>
2019-05-28 23:18:00 +0200

    mejoras imagenes

--- bootstrap_ex.py
+++ bootstrap_ex.py
@@ -13,107 +13,120 @@ from tfg_utils import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-block_size = 200 #let's use a different according to acf
 
-bs1 = MovingBlockBootstrap(block_size,segment1)
-
-hurst_segment_1 = []
-spectrum_segment_1 = []
-
-for pos_data, kwdata in bs1.bootstrap(100):
-    print(pos_data)
+def bootstrap_index(segment1,segment2,segment3,segment4):
+    """
     
-    print(kwdata)
+    """
+    np.random.seed('1947')
     
-    boots_series = pos_data[0]
+    block_size = 200 #let's use a different according to acf
     
-    #calcular hurst y spectrum para este remuestreo
-    hurst_segment_1.append(hurst(boots_series))
-    spectrum_segment_1.append(spectrum1f(boots_series))
-  
-bs2 = MovingBlockBootstrap(block_size,segment2)
-
-hurst_segment_2 = []
-spectrum_segment_2 = []
-
-for pos_data, kwdata in bs2.bootstrap(100):
-    print(pos_data)
+    bs1 = MovingBlockBootstrap(block_size,segment1)
     
-    print(kwdata)
+    hurst_segment_1 = []
+    spectrum_segment_1 = []
     
-    boots_series = pos_data[0]
+    for pos_data, kwdata in bs1.bootstrap(100):
+        print(pos_data)
+        
+        print(kwdata)
+        
+        boots_series = pos_data[0]
+        
+        #calcular hurst y spectrum para este remuestreo
+        hurst_segment_1.append(hurst(boots_series))
+        spectrum_segment_1.append(spectrum1f(boots_series)[-2])
+      
+    bs2 = MovingBlockBootstrap(block_size,segment2)
     
-    #calcular hurst y spectrum para este remuestreo
-    hurst_segment_2.append(hurst(boots_series))
-    spectrum_segment_2.append(spectrum1f(boots_series))
-
-bs3 = MovingBlockBootstrap(block_size,segment3)
-
-hurst_segment_3 = []
-spectrum_segment_3 = []
-
-for pos_data, kwdata in bs3.bootstrap(100):
-    print(pos_data)
+    hurst_segment_2 = []
+    spectrum_segment_2 = []
     
-    print(kwdata)
+    for pos_data, kwdata in bs2.bootstrap(100):
+        print(pos_data)
+        
+        print(kwdata)
+        
+        boots_series = pos_data[0]
+        
+        #calcular hurst y spectrum para este remuestreo
+        hurst_segment_2.append(hurst(boots_series))
+        spectrum_segment_2.append(spectrum1f(boots_series)[-2])
     
-    boots_series = pos_data[0]
+    bs3 = MovingBlockBootstrap(block_size,segment3)
     
-    #calcular hurst y spectrum para este remuestreo
-    hurst_segment_3.append(hurst(boots_series))
-    spectrum_segment_3.append(spectrum1f(boots_series))
+    hurst_segment_3 = []
+    spectrum_segment_3 = []
     
-bs4 = MovingBlockBootstrap(block_size,segment4)
-
-hurst_segment_4 = []
-spectrum_segment_4 = []
-
-for pos_data, kwdata in bs4.bootstrap(100):
-    print(pos_data)
+    for pos_data, kwdata in bs3.bootstrap(100):
+        print(pos_data)
+        
+        print(kwdata)
+        
+        boots_series = pos_data[0]
+        
+        #calcular hurst y spectrum para este remuestreo
+        hurst_segment_3.append(hurst(boots_series))
+        spectrum_segment_3.append(spectrum1f(boots_series)[-2])
+        
+    bs4 = MovingBlockBootstrap(block_size,segment4)
     
-    print(kwdata)
+    hurst_segment_4 = []
+    spectrum_segment_4 = []
     
-    boots_series = pos_data[0]
+    for pos_data, kwdata in bs4.bootstrap(100):
+        print(pos_data)
+        
+        print(kwdata)
+        
+        boots_series = pos_data[0]
+        
+        #calcular hurst y spectrum para este remuestreo
+        hurst_segment_4.append(hurst(boots_series))
+        spectrum_segment_4.append(spectrum1f(boots_series)[-2])
+
     
-    #calcular hurst y spectrum para este remuestreo
-    hurst_segment_4.append(hurst(boots_series))
-    spectrum_segment_4.append(spectrum1f(boots_series))
-
-
-#Representación mediante histograma del análisis estadístico mediante bootstrap del exponente de Hurst
-plt.figure()
-plt.hist(hurst_segment_1, alpha = 0.5, label = ('Segmento 1'))
-plt.hist(hurst_segment_2, alpha = 0.2, label = ('Segmento 2'))
-plt.hist(hurst_segment_3, alpha = 0.3, label = ('Segmento 3'))
-plt.hist(hurst_segment_4, alpha = 0.4, label = ('Segmento 4'))
-plt.legend()
-suptitle('Bootstrap Exponente de Hurst')
-title(k)
-
-
-#Representación mediante histograma del análisis estadístico mediante bootstrap de spectrum
-plt.figure()
-plt.hist(spectrum_segment_1[-2], alpha = 0.5, label = ('Segmento 1'))
-plt.hist(spectrum_segment_2[-2], alpha = 0.2, label = ('Segmento 2'))
-plt.hist(spectrum_segment_3[-2], alpha = 0.3, label = ('Segmento 3'))
-plt.hist(spectrum_segment_4[-2], alpha = 0.4, label = ('Segmento 4'))
-plt.legend()
-suptitle('Bootstrap para Spectrum')
-title(k)
-
-
-#Reoresentación de la desviación del exponente de Hurst
-plt.figure()
-std = [np.std(hurst_segment_1),np.std(hurst_segment_2),np.std(hurst_segment_3),np.std(hurst_segment_4)]
-std_vector = np.array(std)
-plt.errorbar(np.arange(4),h,std_vector,fmt='o')
-suptitle('Desviación para los exponentes de Hurst')
-title(k)
-
-#Reoresentación de la desviación del exponente de Hurst
-plt.figure()
-std_spect = [np.std(spectrum_segment_1[-2]),np.std(spectrum_segment_2[-2]),np.std(spectrum_segment_3[-2]),np.std(spectrum_segment_4[-2])]
-std_vector_spect = np.array(std_spect)
-plt.errorbar(np.arange(4),spect,std_vector_spect,marker='o')
-suptitle('Desviación para Spectrum')
-title(k)
+    
+    #Representación mediante histograma del análisis estadístico mediante bootstrap del exponente de Hurst
+    plt.figure()
+    subplot(2,2,1)
+    plt.hist(hurst_segment_1, alpha = 0.5, label = ('Segmento 1'))
+    subplot(2,2,2)
+    plt.hist(hurst_segment_2, alpha = 0.2, label = ('Segmento 2'))
+    subplot(2,2,3)
+    plt.hist(hurst_segment_3, alpha = 0.3, label = ('Segmento 3'))
+    subplot(2,2,4)
+    plt.hist(hurst_segment_4, alpha = 0.4, label = ('Segmento 4'))
+    plt.legend()
+    suptitle('Bootstrap Exponente de Hurst')
+    title(k)
+    
+    
+    #Representación mediante histograma del análisis estadístico mediante bootstrap de spectrum
+    plt.figure()
+    plt.hist(spectrum_segment_1, alpha = 0.5, label = ('Segmento 1'))
+    plt.hist(spectrum_segment_2, alpha = 0.2, label = ('Segmento 2'))
+    plt.hist(spectrum_segment_3, alpha = 0.3, label = ('Segmento 3'))
+    plt.hist(spectrum_segment_4, alpha = 0.4, label = ('Segmento 4'))
+    plt.legend()
+    suptitle('Bootstrap para Spectrum')
+    title(k)
+    
+    
+    
+    #Reoresentación de la desviación del exponente de Hurst
+    plt.figure()
+    std = [np.std(hurst_segment_1),np.std(hurst_segment_2),np.std(hurst_segment_3),np.std(hurst_segment_4)]
+    std_vector = np.array(std)
+    plt.errorbar(np.arange(4),h,std_vector,marker='o')
+    suptitle('Desviación para los exponentes de Hurst')
+    title(k)
+    
+    #Reoresentación de la desviación de spectrum
+    plt.figure()
+    std_spect = [np.std(spectrum_segment_1),np.std(spectrum_segment_2),np.std(spectrum_segment_3),np.std(spectrum_segment_4)]
+    std_vector_spect = np.array(std_spect)
+    plt.errorbar(np.arange(4),spect,std_vector_spect,marker='o')
+    suptitle('Desviación para Spectrum')
+    title(k)
--- plot_idx.py
+++ plot_idx.py
@@ -5,136 +5,150 @@ Created on Fri Apr  5 12:49:02 2019
 
 @author: Lau
 """
+
 from tfg_utils import *
 from ts_fractals_tools import *
+from arch.bootstrap import MovingBlockBootstrap
+import numpy as np
+import matplotlib.pyplot as plt
 
-idx = get_indices()
 
-for k in idx:
-#for k in ['AEX']:
-    ii = idx[k] #array index, k=noun index
-    fechas = ii[0]  #date index
-    price = ii[1]   #price index
+def plot_idx_func():
+    """
+    """
     
-    #reverse fechas
-    fechas = fechas[-1::-1]
+    idx = get_indices()
     
-    #to reverse data
-    price = price[-1::-1]
+    for k in idx:
+    #for k in ['AEX']:
+        ii = idx[k] #array index, k=noun index
+        fechas = ii[0]  #date index
+        price = ii[1]   #price index
+        
+        #reverse fechas
+        fechas = fechas[-1::-1]
+        
+        #to reverse data
+        price = price[-1::-1]
+        
+        t = np.arange(len(price))
     
-    t = np.arange(len(price))
-
-    #get date position
-    pos = []
+        #get date position
+        pos = []
+        
+        pos.append(np.where(fechas == '05.01.2001')[0])
+        pos.append(np.where(fechas == '02.01.2004')[0])
+        pos.append(np.where(fechas == '02.01.2008')[0])
+        pos.append(np.where(fechas == '02.01.2014')[0])
+        pos.append(np.where(fechas == '02.01.2019')[0])
+        pos = np.array(pos)
+        pos = pos.flatten()
+        #pos = pos[-1::-1]
+        
+        print(pos, k)
     
-    pos.append(np.where(fechas == '05.01.2001')[0])
-    pos.append(np.where(fechas == '02.01.2004')[0])
-    pos.append(np.where(fechas == '02.01.2008')[0])
-    pos.append(np.where(fechas == '02.01.2014')[0])
-    pos.append(np.where(fechas == '02.01.2019')[0])
-    pos = np.array(pos)
-    pos = pos.flatten()
-    #pos = pos[-1::-1]
+        #plot each index
+        plt.figure()
+        plt.plot(price, label = k)
+        print(k)
+        plt.legend(loc='upper right')
+        plt.xticks(pos,fechas[pos], size = 'small', rotation = 45)
+        title('Representación índices')
+        xlabel('Periodos más representativos')
+        ylabel('Precios de cierre')
+     
+        #index with colours
+        plt.figure()
+        plt.plot(t[:pos[0]],price[0:pos[0]])
+        plt.plot(t[pos[0]:pos[1]],price[pos[0]:pos[1]])
+        plt.plot(t[pos[1]:pos[2]],price[pos[1]:pos[2]])
+        plt.plot(t[pos[2]:pos[3]],price[pos[2]:pos[3]])
+        plt.plot(t[pos[3]:pos[4]],price[pos[3]:pos[4]])
+        plt.xticks(pos,fechas[pos], size = 'small', rotation = 45)
+        suptitle('Representación índices')
+        title(k)
+        xlabel('Periodos más representativos')
+        ylabel('Precios de cierre')
+        plt.tight_layout()
+        
+        
+        #segment each index
+        segment1 = price[:pos[1]]
+        segment2 = price[pos[1]+1:pos[2]]
+        segment3 = price[pos[2]+1:pos[3]]
+        segment4 = price[pos[3]+1:pos[4]]
+        
+        #xcorr for segment1
+        x = segment1
+        y = segment1
+        plt.figure()
+        xcorr1= plt.xcorr(x, y, normed=True, usevlines=True, maxlags=None)
+        #plt.plot(xcorr1[0],xcorr1[1],'-')
+        #title('Autocorrelación segmento 1')
+        
+        #xcorr for segment2
+        x = segment2
+        y = segment2
+        plt.figure()
+        xcorr2= plt.xcorr(x, y, normed=True, usevlines=True, maxlags=None)
+        #plt.plot(xcorr2[0],xcorr2[1],'-')
+        #title('Autocorrelación segmento 2')
     
-    print(pos, k)
-
-    #plot each index
-    plt.figure()
-    plt.plot(price, label = k)
-    print(k)
-    plt.legend(loc='upper right')
-    plt.xticks(pos,fechas[pos], size = 'small', rotation = 45)
-    title('Representación índices')
-    xlabel('Periodos más representativos')
-    ylabel('Precios de cierre')
- 
-    #index with colours 
-    plt.plot(t[:pos[0]],price[0:pos[0]])
-    plt.plot(t[pos[0]:pos[1]],price[pos[0]:pos[1]])
-    plt.plot(t[pos[1]:pos[2]],price[pos[1]:pos[2]])
-    plt.plot(t[pos[2]:pos[3]],price[pos[2]:pos[3]])
-    plt.plot(t[pos[3]:pos[4]],price[pos[3]:pos[4]])
-    plt.xticks(pos,fechas[pos], size = 'small', rotation = 45)
-    suptitle('Representación índices')
-    title(k)
-    xlabel('Periodos más representativos')
-    ylabel('Precios de cierre')
+        #xcorr for segment3
+        x = segment3
+        y = segment3
+        plt.figure()
+        xcorr3= plt.xcorr(x, y, normed=True, usevlines=True, maxlags=None)
+        #plt.plot(xcorr3[0],xcorr3[1],'-')
+        #title('Autocorrelación segmento 3')
     
+        #PREGUNTAR SI EN LA MEMORIA, EN EL CASO DE QUERER LAS AUTOCORRELACIONES SEPARADAS, PONGO LA FIGURA QUE SALE NEGRA O EL CONTORNO SOLO
+        #xcorr for segment4
+        x = segment4
+        y = segment4
+        plt.figure()
+        xcorr4= plt.xcorr(x, y, normed=True, usevlines=True, maxlags=None)
+        #plt.plot(xcorr4[0],xcorr4[1],'-')
+        #title('Autocorrelación segmento 4')
     
-    #segment each index
-    segment1 = price[:pos[1]]
-    segment2 = price[pos[1]+1:pos[2]]
-    segment3 = price[pos[2]+1:pos[3]]
-    segment4 = price[pos[3]+1:pos[4]]
-    
-    #xcorr for segment1
-    x = segment1
-    y = segment1
-    plt.figure()
-    xcorr1= plt.xcorr(x, y, normed=True, usevlines=True, maxlags=None)
-    plt.plot(xcorr1[0],xcorr1[1],'-')
-    title('Autocorrelación segmento 1')
-    
-    #xcorr for segment2
-    x = segment2
-    y = segment2
-    plt.figure()
-    xcorr2= plt.xcorr(x, y, normed=True, usevlines=True, maxlags=None)
-    plt.plot(xcorr2[0],xcorr2[1],'-')
-    title('Autocorrelación segmento 2')
-
-    #xcorr for segment3
-    x = segment3
-    y = segment3
-    plt.figure()
-    xcorr3= plt.xcorr(x, y, normed=True, usevlines=True, maxlags=None)
-    plt.plot(xcorr3[0],xcorr3[1],'-')
-    title('Autocorrelación segmento 3')
-
-    #PREGUNTAR SI EN LA MEMORIA, EN EL CASO DE QUERER LAS AUTOCORRELACIONES SEPARADAS, PONGO LA FIGURA QUE SALE NEGRA O EL CONTORNO SOLO
-    #xcorr for segment4
-    x = segment4
-    y = segment4
-    plt.figure()
-    xcorr4= plt.xcorr(x, y, normed=True, usevlines=True, maxlags=None)
-    plt.plot(xcorr4[0],xcorr4[1],'-')
-    title('Autocorrelación segmento 4')
-
-    #NO BORRAR, PREGUNTAR SI ME SIRVE O ES MEJOR POR SEPARADO
-    #Representación de todas las autocorrelaciones juntas
-    plt.figure()
-    plt.plot(xcorr1[0],xcorr1[1],'-', label = ('Autocorrelación segmento 1'))
-    plt.plot(xcorr2[0],xcorr2[1],'-', label = ('Autocorrelación segmento 2'))
-    plt.plot(xcorr3[0],xcorr3[1],'-', label = ('Autocorrelación segmento 3'))
-    plt.plot(xcorr4[0],xcorr4[1],'-', label = ('Autocorrelación segmento 4'))
-    title(k)
-    suptitle("Autocorrelación")
-    plt.legend(loc='upper right')
-    
-    #hurst
-    hurst1 = hurst(segment1)
-    hurst2 = hurst(segment2)
-    hurst3 = hurst(segment3)
-    hurst4 = hurst(segment4)
-    h = np.array([hurst1,hurst2,hurst3,hurst4])
-    plt.figure()
-    plt.plot(h,'o', label = k)
-    plt.legend(loc='upper right')
-    suptitle('Exponente de Hurst')
-    title(k)
-    
-    #spectrum, get the last value for w1a
-    spectrum1 = spectrum1f(segment1)
-    w1 = spectrum1[-2]
-    spectrum2 = spectrum1f(segment2)
-    w2 = spectrum2[-2]
-    spectrum3 = spectrum1f(segment3)
-    w3 = spectrum3[-2]
-    spectrum4 = spectrum1f(segment4)
-    w4 = spectrum4[-2]
-    spect = np.array([w1,w2,w3,w4])
-    plt.figure()
-    plt.plot(h,'o', label = k)
-    plt.legend(loc='upper right')
-    suptitle('Spectrum')
-    title(k)
+        
+        #Representación de todas las autocorrelaciones juntas
+        plt.figure()
+        plt.plot(xcorr1[0],xcorr1[1],'-', label = ('Autocorrelación segmento 1'))
+        plt.plot(xcorr2[0],xcorr2[1],'-', label = ('Autocorrelación segmento 2'))
+        plt.plot(xcorr3[0],xcorr3[1],'-', label = ('Autocorrelación segmento 3'))
+        plt.plot(xcorr4[0],xcorr4[1],'-', label = ('Autocorrelación segmento 4'))
+        title(k)
+        suptitle("Autocorrelación")
+        plt.legend(loc='upper right')
+        
+        #hurst
+        hurst1 = hurst(segment1)
+        hurst2 = hurst(segment2)
+        hurst3 = hurst(segment3)
+        hurst4 = hurst(segment4)
+        h = np.array([hurst1,hurst2,hurst3,hurst4])
+        #plt.figure()
+        #plt.plot(h,'o', label = k)
+        #plt.legend(loc='upper right')
+        #suptitle('Exponente de Hurst')
+        #title(k)
+        
+        #spectrum, get the last value for w1a
+        spectrum1 = spectrum1f(segment1)
+        w1 = spectrum1[-2]
+        spectrum2 = spectrum1f(segment2)
+        w2 = spectrum2[-2]
+        spectrum3 = spectrum1f(segment3)
+        w3 = spectrum3[-2]
+        spectrum4 = spectrum1f(segment4)
+        w4 = spectrum4[-2]
+        spect = np.array([w1,w2,w3,w4])
+        #plt.figure()
+        #plt.plot(h,'o', label = k)
+        #plt.legend(loc='upper right')
+        #suptitle('Spectrum')
+        #title(k)
+        
+        bootstrap_index(segment1,segment2,segment3,segment4)
+       
